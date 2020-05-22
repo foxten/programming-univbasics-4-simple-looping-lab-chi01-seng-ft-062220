88a1eb61f49f11b8ac4b5c9274b26b840c773d7d
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-22 04:04:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,9 +23,13 @@ def output_array (array)
 end
 
 def return_string_array (array)
+  new_array = []
   i = 0
+  new_array.push(
     while array[i] do
     puts array[i].to_s
     i+=1
   end
+  )
+  return new_array
 end
